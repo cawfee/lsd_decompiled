b52f20fa1ee7e02e5da0dff9aafa5003bdf4511c
cawfee <80633385+cawfee@users.noreply.github.com>
2025-09-07 02:35:08 +0900

    missing files

---
+++ src/helper_unk_1.c
@@ -0,0 +1,13 @@
+#include "helper_unk_1.h"
+
+// gp
+INCLUDE_ASM("asm/nonmatchings/helper_unk_1", func_8002CC1C);
+
+// gp
+INCLUDE_ASM("asm/nonmatchings/helper_unk_1", helper_1_get_crescendo_time_mod);
+
+INCLUDE_ASM("asm/nonmatchings/helper_unk_1", helper_1_set_entity);
+
+INCLUDE_ASM("asm/nonmatchings/helper_unk_1", func_8002CC84);
+
+INCLUDE_ASM("asm/nonmatchings/helper_unk_1", helper_1_update_entity);
---
+++ src/helper_unk_1.h
@@ -0,0 +1,6 @@
+#ifndef LSD_HELPER_UNK_1_H
+#define LSD_HELPER_UNK_1_H
+
+#include "common.h"
+
+#endif // LSD_HELPER_UNK_1_H
